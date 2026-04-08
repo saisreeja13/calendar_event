2e41b2863ad5093b211b63f2adfe9e742ab0dd1d
saisreeja13 <mvnsreeja@gmail.com>
2026-04-08 21:40:08 +0530

    Add files via upload
    Added calendar event app code

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Calendar from "@/components/Calendar";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-200 flex items-center justify-center p-4">
+      <Calendar />
+    </main>
+  );
+}
